c8bcb0a5c1cd13a4a43eb85854593bdca1ffe362
Miller Peterson <miller.peterson@gmail.com>
2014-02-25 21:43:22 -0500

    Add some simple logic for rooms
    Signed-off-by: Miller Peterson <miller.peterson@gmail.com>

--- world.js
+++ world.js
@@ -1,29 +1,74 @@
-//var lPad = require('../phi-launchpad-js/launchpad')
+var lPad = require('../phi-launchpad-js/launchpad');
+var midiInt = require('../phi-launchpad-js/node_midi_interface');
 
 var World = function() {
   this.areas = new Array();
 };
 
-var Area = function(rows, columns) {
-};
+var Area = function() { };
 
 Area.prototype.init = function(rows, columns) {
   this.rooms = new Array(rows);
   for (var r = 0; r < this.rooms.length; r++)  {
     this.rooms[r] = new Array(columns);
     for (var c = 0; c < this.rooms[r].length; c++) {
-      this.rooms[r][c] = new Room(r, c);
+      this.rooms[r][c] = new Room(r, c, this);
     }
   }
 }
 
-var Room = function(row, col) {
+Area.prototype.rows = function() {
+  return this.rooms.length;
+}
+
+Area.prototype.cols = function() {
+  return this.rooms[0].length;
+}
+
+Area.prototype.draw = function(lPad) {
+  var room, numWalls;
+  for (var r = 0; r < this.rooms.length; r++) {
+    for (var c = 0; c < this.rooms[r].length; c++) {
+      room = this.rooms[r][c];
+      numWalls = room.walls.length;
+      lPad.setLed(r, c, [numWalls, numWalls]);
+    }
+  }
+}
+
+var Room = function(row, col, area) {
   // list of vectors
   this.walls = [ // up, right, down, left
     [-1, 0], [0, 1], [1, 0], [0, -1]
   ];
   this.pos = [row, col]; // row, column
+  this.area = area;
 };
 
-var testArea = new Area(9, 9);
-testArea.init();
+Room.prototype.neighbors = function() {
+  var n = [];
+  if (this.pos[0] > 0) {
+    n.push([this.pos[0] - 1, this.pos[1]]); // Up
+  }
+  if (this.pos[0] < this.area.rows()) {
+    n.push([this.pos[0] + 1, this.pos[1]]); // Down
+  }
+  if (this.pos[1] > 0) {
+    n.push([this.pos[0], this.pos[1] - 1]); // Left
+  }
+  if (this.pos[1] < this.area.cols()) {
+    n.push([this.pos[0], this.pos[1] + 1]); // Right
+  }
+  // B A B A SELECT START
+  return n;
+}
+
+var mInt = new midiInt.midi();
+var pad = new lPad.launchpad();
+pad.init(mInt);
+pad.resetDevice();
+
+var testArea = new Area();
+testArea.init(9, 9);
+console.log(testArea.rooms[4][4].neighbors());
+//testArea.draw(pad);
