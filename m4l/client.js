6afdd16f36a5c4a946d786d7be432443ff1a7122
Miller Peterson <miller.peterson@gmail.com>
2014-05-12 21:43:59 -0400

    Change M4L client to use UDP instead of TCP
    Signed-off-by: Miller Peterson <miller.peterson@gmail.com>

--- m4l/client.js
+++ m4l/client.js
@@ -1,31 +1,25 @@
-var net = require('net');
+var dgram = require('dgram');
 
-var M4LClient = function(serverIP, serverPort) {
+var M4LClient = function(serverAddr, serverPort) {
   var that = this;
-  if (typeof(serverIP) === 'undefined') {
-    serverIP = '127.0.0.1';
+  if (typeof(serverAddr) === 'undefined') {
+    serverAddr = 'localhost';
   }
   if (typeof(serverPort) === 'undefined') {
     serverPort = 6000;
   }
-  this.serverIP = serverIP;
+  this.serverAddr = serverAddr;
   this.serverPort = serverPort;
-  var client = new net.Socket();
-  client.setNoDelay(true);
-  client.on('error', function(error) {
-    console.log(error);
+  this.udp = new dgram.createSocket("udp4");
+  this.udp.on('error', function(err) {
+    console.log('UDP Error: ' + err.stack);
   });
-  this.client = client;
-}
+};
 
 M4LClient.prototype.sendMsg = function(message) {
-  var c = this.client;
-  this.client.connect(this.serverPort, this.serverIP, function() {
-    c.write(message, 'utf8', function() {
-      c.end();
-    });
-  });
-}
+  var buff = new Buffer(message);
+  this.udp.send(buff, 0, buff.length, this.serverPort, this.serverAddr);
+};
 
 var client = M4LClient;
 module.exports.client = client;
