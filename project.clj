7a71303d3e6ed2a78e080d8085ca950709ae7c8d
Miller Peterson <miller.peterson@gmail.com>
2017-04-30 20:29:06 -0400

    UDP channels via core.async

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [overtone/osc-clj "0.7.1"]
+                 [org.clojure/core.async "0.3.442"]
+                 [org.clojure/data.json "0.2.6"]
                  [org.clojure/math.combinatorics "0.1.3"]]
   :main ^:skip-aot sound-maze-game.core
   :target-path "target/%s"
---
+++ src/sound_maze_game/udp.clj
@@ -0,0 +1,41 @@
+(ns sound-maze-game.udp
+  (:require [clojure.core.async :as async :refer [go >! <! chan]])
+  (:import (java.net DatagramPacket DatagramSocket InetAddress)))
+
+(defn make-socket
+  "Make a datagram socket."
+	([] (new DatagramSocket))
+	([port] (new DatagramSocket port)))
+
+(defn send-data
+  "Send data over a socket to a particular ip/port combo."
+  [send-socket ip port data]
+  (let [ipaddress (InetAddress/getByName ip)
+        send-packet (new DatagramPacket (.getBytes data) (.length data) ipaddress port)]
+    (.send send-socket send-packet)))
+
+(defn receive-data
+  "Receive data over a socket."
+  [receive-socket]
+  (let [receive-data (byte-array 1024),
+       receive-packet (new DatagramPacket receive-data 1024)]
+    (.receive receive-socket receive-packet)
+    (new String (.getData receive-packet) 0 (.getLength receive-packet))))
+
+(defn make-receive-channel
+  "Make a channel that receives data over a given port."
+  [receive-port]
+	(let [receive-socket (make-socket receive-port)
+        receive-channel (chan)]
+		(go (while true
+          (>! receive-channel (receive-data receive-socket))))
+    receive-channel))
+
+(defn make-send-channel
+  "Make a function that sends data to a given ip/port."
+  [ip port]
+	(let [send-socket (make-socket)
+        send-channel (chan)]
+    (go (while true
+          (send-data send-socket ip port (<! send-channel))))
+    send-channel))
