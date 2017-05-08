21fb29ed29ca91d3ccb479110a8d1fde7ca39c50
Miller Peterson <miller.peterson@gmail.com>
2017-05-08 14:38:28 -0400

    Remove redundant max/msp from names
    Rework methods a bit.

--- src/sound_maze_game/maxmsp.clj
+++ src/sound_maze_game/maxmsp.clj
@@ -3,22 +3,22 @@
             [sound-maze-game.udp :as udp]
             [clojure.data.json :as json]))
 
-(defn make-maxmsp-receive-channel
+(defn make-receive-channel
   "Channel for receiving messages from max/msp."
   [port]
   (udp/make-receive-channel port))
 
-(defn make-maxmsp-send-channel
+(defn make-send-channel
   "Channel for sending messages to max/msp."
   [port]
   (udp/make-send-channel "localhost" port))
 
-(defn send-to-max
+(defn max-serialized
   "Send a message on a max/msp channel."
-  [channel msg]
-  (>!! channel (json/write-str msg)))
+  [msg]
+  (json/write-str msg))
 
-(defn next-max-msg
+(defn max-deserialized
   "Receive a message from a max/msp channel."
-  [channel]
-  (json/read-str (<!! channel)))
+  [msg]
+  (json/read-str msg))
