1de5ed55e34448005ff261c19ed23cc68fc24a8f
Miller Peterson <miller.peterson@gmail.com>
2017-05-08 14:38:59 -0400

    Basic game loop

--- src/sound_maze_game/loop.clj
+++ src/sound_maze_game/loop.clj
@@ -1,49 +1,54 @@
 (ns sound-maze-game.loop
-  (require [sound-maze-game.maze :as maze]
-           [sound-maze-game.osc :as osc]))
+  (:require [clojure.core.async :as async :refer [go-loop chan <!]]
+            [clojure.string :as string :refer [trim]]
+            [sound-maze-game.maze :as maze]
+            [sound-maze-game.demo-mazes :as demo-mazes]
+            [sound-maze-game.maxmsp :as maxmsp]))
 
-(def osc-server (osc/server))
-(def osc-cli (osc/client))
 (def game-state-history (atom []))
 
-(defn start-handling-osc-events!
-  [server]
-  (osc/handle! server "/maze-game-events" (fn [msg] (println msg))))
+(defn player
+  "A game world player."
+  [pos]
+  (hash-map :pos pos
+            :movement-intention nil))
 
-;; (defn notify-as-json!
-;;   [cli message]
-;;   (osc/send-as-json! cli message))
+(defn start-game-state
+  "Starting state for a game with a given maze and player."
+  [maze player]
+  (hash-map :maze maze
+            :player player
+            :turn 0))
 
-;; (defn player
-;;   "A game world player."
-;;   [pos]
-;;   (hash-map :pos pos
-;;             :movement-intentionmedi nil))
+(defn state-after-action
+  "Return the state after processing a game action."
+  [game-state action]
+  game-state)
 
-;; (defn start-game-state
-;;   "Starting state for a game with a given maze and player."
-;;   [maze player]
-;;   (hash-map :maze maze
-;;             :player player
-;;             :turn 0))
+(defn handle-action!
+  "Handles an action, updating the game state history.
+   Returns the updated game state."
+  [game-state action-data]
+  (let [action (maxmsp/max-deserialized action-data)
+        new-state (state-after-action game-state action)]
+    (println "handle action" action new-state)
+    (swap! game-state-history #(conj % [action, new-state]))
+    new-state))
 
-;; (defn update-game-state!
-;;   [action new-state]
-;;   (swap game-state-history #(conj % {:action action
-;;                                      :state state})))
+(defn start-game-loop
+  "Start a game loop running. Returns a channel for telling the
+   loop to stop."
+  [start-state from-max-chan]
+  (go-loop [state start-state]
+    (let [action-data (trim (<! from-max-chan))]
+      (println "#" action-data "#")
+      (if (not (= action-data "quit!"))
+        (let [new-state (handle-action! state action-data)]
+          (recur new-state))))))
 
-;; (defn handle-action
-;;   "Return the state after handling a game action."
-;;   [game-state action]
-;;   game-state)
-
-;; (defn tick!
-;;   "Advance one step in the simulation."
-;;   [game-state]
-;;   game-state)
-
-;; (defn start-game!
-;;   [maze player]
-;;   (let [start-state (start-game-state maze player)
-;;     (osc-handle )))
-
+(comment
+  (def max-send-chan (maxmsp/make-send-channel 7605))
+  (def max-recv-chan (maxmsp/make-receive-channel 7607))
+  (def start-state (start-game-state demo-mazes/mvi1-maze (player [0 0])))
+  (start-game-loop start-state max-recv-chan)
+)
