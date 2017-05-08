ae0157d2694aea5f4a83293e2a0beb976598bc4a
Miller Peterson <miller.peterson@gmail.com>
2017-05-08 14:39:08 -0400

    Remove dumb stuff from core

--- src/sound_maze_game/core.clj
+++ src/sound_maze_game/core.clj
@@ -1,7 +1,1 @@
-(ns sound-maze-game.core
-  (:require [sound-maze-game.colors :as colors]))
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println colors/red))
+(ns sound-maze-game.core)
